fc6788474912c9f66f3caa563360eabf50e4f5e3
Toni <tonilupianezroman@gmail.com>
2025-07-26 17:11:25 +0200

    Update for deploy

--- FinancialTrackerFrontEndAngular/src/environments/environment.prod.ts
+++ FinancialTrackerFrontEndAngular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // Cambiar la URL del backend por la URL de producción cuando esté desplegado
-  apiUrl: 'myfinancialtracker.netlify.app', // Cambiar por la URL real de tu backend
+  // Cambiar por la URL real de tu backend desplegado
+  apiUrl: 'https://tu-backend-url.com', // Ejemplo: https://financial-tracker-backend.herokuapp.com
   twelvedataApiKey: 'b18b371800b24fceb2f2439b2e6c1c60',
   exchangerateApiKey: '6ef67e7d3c6e02e659707acc'
 };

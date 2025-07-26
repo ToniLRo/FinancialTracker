112af1c9b4e2f36a91044f0b2a8fcd421f01f68a
ToniLR999 <tonilupianezroman@gmail.com>
2025-07-26 20:02:12 +0200

    update for deploy backend on railway

--- FinancialTrackerFrontEndAngular/src/environments/environment.prod.ts
+++ FinancialTrackerFrontEndAngular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // Cambiar por la URL real de tu backend desplegado
-  apiUrl: 'https://tu-backend-url.com', // Ejemplo: https://financial-tracker-backend.herokuapp.com
+  apiUrl: 'financialtracker-production.up.railway.app', // Ejemplo: https://financial-tracker-backend.herokuapp.com
   twelvedataApiKey: 'b18b371800b24fceb2f2439b2e6c1c60',
   exchangerateApiKey: '6ef67e7d3c6e02e659707acc'
 };

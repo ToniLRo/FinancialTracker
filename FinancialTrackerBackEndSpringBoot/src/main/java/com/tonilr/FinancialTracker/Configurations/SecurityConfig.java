dafd9647ddaa256f7522911b6c1cf5a611dfb4e0
Toni <tonilupianezroman@gmail.com>
2025-07-26 17:04:54 +0200

    Update for deploy

--- FinancialTrackerBackEndSpringBoot/src/main/java/com/tonilr/FinancialTracker/Configurations/SecurityConfig.java
+++ FinancialTrackerBackEndSpringBoot/src/main/java/com/tonilr/FinancialTracker/Configurations/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://myfinancialtracker.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);

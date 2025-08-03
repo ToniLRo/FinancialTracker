2ed9ba2955116af23b89634718ea168090675b10
Toni <tonilupianezroman@gmail.com>
2025-08-03 13:50:12 +0200

    added backend url on security config

--- FinancialTrackerBackEndSpringBoot/src/main/java/com/tonilr/FinancialTracker/Configurations/SecurityConfig.java
+++ FinancialTrackerBackEndSpringBoot/src/main/java/com/tonilr/FinancialTracker/Configurations/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
             )
 
             .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Añadir esta línea
             .requestMatchers("/auth/**").permitAll()
             .requestMatchers("/api/email/**").authenticated()
             .requestMatchers("/marketdata/**").permitAll()

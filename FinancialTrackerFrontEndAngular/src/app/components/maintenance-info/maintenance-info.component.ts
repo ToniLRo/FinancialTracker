c7e065d7948d5e1b0b212d83633ae1c2703b976b
Toni <tonilupianezroman@gmail.com>
2025-08-18 21:28:02 +0200

    New maintenance info component

---
+++ FinancialTrackerFrontEndAngular/src/app/components/maintenance-info/maintenance-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-info',
+  imports: [],
+  templateUrl: './maintenance-info.component.html',
+  styleUrl: './maintenance-info.component.css'
+})
+export class MaintenanceInfoComponent {
+
+}
